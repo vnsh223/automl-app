74727a774a5882388b8c6e614597d2ed0a77b961
vnsh223 <vansh20062206@gmail.com>
2026-02-14 22:40:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ st.title("🚀 AutoML Web Application")
 uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
 if uploaded_file is not None:
-
     df = pd.read_csv(uploaded_file)
 
     st.subheader("Dataset Preview")
@@ -22,14 +21,28 @@ if uploaded_file is not None:
     if st.button("Train Model"):
         X = df.drop(columns=[target])
         y = df[target]
+    if y.dtype == 'object':
+        from sklearn.preprocessing import LabelEncoder
+        le = LabelEncoder()
+        y = le.fit_transform(y)
+
+    model = RandomForestClassifier()
+    model.fit(X, y)
+
+    st.success("Model Trained Successfully")
 
     # Convert categorical columns into numbers
         X = pd.get_dummies(X)
 
     # If target is text → convert it also
-    if y.dtype == 'object':
-        y = pd.factorize(y)[0]
+    if st.button("Train Model"):
 
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    # Convert categorical columns into numbers
+
+    st.success("Model Trained Successfully")
     st.write("Training started... Please wait ⏳")
 
     from sklearn.model_selection import train_test_split
